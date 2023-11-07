b4ad06b8f1683cb81024830f5ce378ea704bf450
DemonLord <120779070+NimithSan@users.noreply.github.com>
2023-11-07 13:26:18 +0700

    ~

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uvicorn
 from routes import login_route, product_route, qrcode_route, register_route,transaction_route,supplier_route,category_route
 from dotenv import load_dotenv
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 load_dotenv()
 
@@ -15,10 +16,6 @@ app.include_router(category_route.router)
 app.include_router(transaction_route.router)
 app.include_router(qrcode_route.router)
 app.include_router(supplier_route.router)
-
-from fastapi.responses import HTMLResponse
-
-app = FastAPI()
 
 # allow to serve static files
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
@@ -44,7 +41,8 @@ async def root():
     return HTMLResponse(html)
 
 
-
+if __name__ == "__main__":
+  uvicorn.run(app,host="0.0.0.0",port="8000")
 
 
 
