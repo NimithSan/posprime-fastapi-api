70cc110a0dbea0c4ba4547b8c218ec75a68ea023
DemonLord <120779070+NimithSan@users.noreply.github.com>
2023-11-07 11:00:44 +0700

    add web root

--- main.py
+++ main.py
@@ -16,8 +16,33 @@ app.include_router(transaction_route.router)
 app.include_router(qrcode_route.router)
 app.include_router(supplier_route.router)
 
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
 # allow to serve static files
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h1>Hello from FastAPI@</h1>
+        </div>
+    </body>
+</html>
+"""
+
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
+
 
 # if __name__ == "__main__":
 #     uvicorn.run(app, host= "192.168.48.1", port=8000)
